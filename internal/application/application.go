26499fc73819195504351577304204491d628ea9
Keita Mizuoch <keitax913@yahoo.co.jp>
2019-04-20 18:00:17 +0900

    Make to handle asset files

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,9 +1,16 @@
 package application
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func SetupGin(controller *PostController) *gin.Engine {
 	g := gin.New()
-	g.GET("/:filename", controller.Get)
+	g.GET("/:basePath", controller.Get)
+	fs := http.FileServer(http.Dir("assets"))
+	g.GET("/:basePath/:filename", func(ctx *gin.Context) {
+		http.StripPrefix("/assets", fs).ServeHTTP(ctx.Writer, ctx.Request)
+	})
 	return g
 }
